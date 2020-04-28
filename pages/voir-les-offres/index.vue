a83e8d342aca4642bb8ef5528e855c12e48fad57
Nathalie <nathaliehenrion@hotmail.com>
2020-04-28 17:24:55 +0200

    ajout variable login

--- pages/voir-les-offres/index.vue
+++ pages/voir-les-offres/index.vue
@@ -1,33 +1,63 @@
 <template>   
     <div>
         <client-only>
-        <div class="table-container" v-if="offers">
-            <div v-if="offers.length>0">
-              <h2>Les offres en cours</h2>
-                  <table>
-                      <thead>
-                          <tr>
-                              <th>Titre</th>
-                              <th>Description</th>
-                              <th>Catégorie</th>
-                              <th>Date d'expiration de l'offre</th>
-                              <th>Membre</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr v-for="offer in offers">
-                              <td><router-link tag="a" to="/" exact>{{ offer.title }}</router-link></td>
-                              <td>{{ offer.description }}</td>
-                              <td><router-link tag="a" :to="{ name: 'category-id', params: {id:offer.category.id}}" exact>{{ offer.category.title }}</router-link></td>
-                              <td>{{ offer.expirationDate }}</td>
-                              <td><router-link tag="a" :to="{ name:'member-id', params: { id:offer.member.id }}" exact>{{ offer.member.pseudo }}</router-link></td>
-                          </tr>
-                      </tbody>
-                  </table>
-            </div> 
-            <div v-else>
-                <h3>Il n'y a pas d'offre en cours.</h3>
-            </div>   
+        <div v-if="login">
+            <div class="table-container" v-if="offers">
+                <div v-if="offers.length>0">
+                  <h2>Les offres en cours</h2>
+                      <table>
+                          <thead>
+                              <tr>
+                                  <th>Titre</th>
+                                  <th>Description</th>
+                                  <th>Catégorie</th>
+                                  <th>Date d'expiration de l'offre</th>
+                                  <th>Membre</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr v-for="offer in offers">
+                                  <td><router-link tag="a" to="/" exact>{{ offer.title }}</router-link></td>
+                                  <td>{{ offer.description }}</td>
+                                  <td><router-link tag="a" :to="{ name: 'category-id', params: {id:offer.category.id}}" exact>{{ offer.category.title }}</router-link></td>
+                                  <td>{{ offer.expirationDate }}</td>
+                                  <td><router-link tag="a" :to="{ name:'member-id', params: { id:offer.member.id }}" exact>{{ offer.member.pseudo }}</router-link></td>
+                              </tr>
+                          </tbody>
+                      </table>
+                </div> 
+                <div v-else>
+                    <h3>Il n'y a pas d'offre en cours.</h3>
+                </div>   
+            </div>
+        </div>
+        <div v-else>
+            <div class="table-container" v-if="offers">
+                <div v-if="offers.length>0">
+                  <h2>Les offres en cours</h2>
+                      <table>
+                          <thead>
+                              <tr>
+                                  <th>Titre</th>
+                                  <th>Description</th>
+                                  <th>Catégorie</th>
+                                  <th>Date d'expiration de l'offre</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr v-for="offer in offers">
+                                  <td><router-link tag="a" to="/" exact>{{ offer.title }}</router-link></td>
+                                  <td>{{ offer.description }}</td>
+                                  <td><router-link tag="a" :to="{ name: 'category-id', params: {id:offer.category.id}}" exact>{{ offer.category.title }}</router-link></td>
+                                  <td>{{ offer.expirationDate }}</td>
+                              </tr>
+                          </tbody>
+                      </table>
+                </div> 
+                <div v-else>
+                    <h3>Il n'y a pas d'offre en cours.</h3>
+                </div>   
+            </div>
         </div>
         </client-only>
     </div>
@@ -41,7 +71,8 @@ export default {
     layout: 'withCategories',
     data() {
         return {
-            offer: Object
+            offer: Object,
+            login:true
         }
     },
     apollo: {
