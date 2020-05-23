af482a06586797eca28c6e390f8ad64c65b272e8
Nathalie <nathaliehenrion@hotmail.com>
2020-05-23 16:01:45 +0200

    ajout requete article + modifs header

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,32 @@
 <template>  
-  <div>
-  
-  </div>
+    <div>
+        <client-only>
+            <div class="table-container" v-if="articles" v-for="article in articles">
+  {{ article.title }}
+  {{ article.content }}
+            </div>
+        </client-only>
+    </div>
 </template>  
+<script> 
+import articlesQuery from '~/apollo/queries/article/articles'
+
+export default {  
+    data() {
+        return {
+            article: Object,    
+        }
+    },
+    apollo: {
+        articles: {
+            prefetch: true,
+            query: articlesQuery
+        }
+    },
+    computed: {
+        username() {
+            return this.$store.getters['auth/username']
+        }
+    },
+}
+</script>
